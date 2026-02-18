e81ca6c8a6196d7e340649d98e708a5d8cce116c
Tristan Hascoet <tristan@yoichi.r-ccs27.riken.jp>
2026-02-18 21:45:49 +0900

    Update compile and run script to also compile scale

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -5,10 +5,17 @@ ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SCALE_DIR="$ROOT_DIR/scale/scale-letkf/scale"
 TEST_DIR="$ROOT_DIR/test/SC23"
 
+cd "$ROOT_DIR/scale/scale-rm/src"
+make -j
+
 cd "$SCALE_DIR"
 make
 
+cd "$ROOT_DIR"
+cp scale/scale-letkf/scale/letkf/letkf test/SC23/bin/letkf
+
 cd "$TEST_DIR"
 ./exec_timed.sh
+
 
 cd "$ROOT_DIR"
